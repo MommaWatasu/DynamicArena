d9d36001240e4b6b614506f2fc6ea255d14b5eb3
MommaWatasu <ascendwatson@gmail.com>
2024-12-28 10:43:08 +0900

    [add function or file]create mainmenu
    add fobutton to quit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use bevy::prelude::*;
+#[allow(unused_imports)]
+use bevy::{
+    prelude::*,
+    window::{
+        Monitor,
+        PrimaryWindow,
+        WindowMode,
+    }
+};
 
 mod mainmenu;
 
-const GAMETITLE: &str = "Bevy Game Template";
-const WINDOW_SIZE: Vec2 = Vec2::new(800.0, 600.0);
+const GAMETITLE: &str = "DynamicArena";
 const BACGROUND_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
-const PATH_FONT: &str = "fonts/DotGothic16-Regular.ttf";
-const PATH_IMAGE: &str = "images/";
-const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
+const PATH_REGULAR_FONT: &str = "fonts/static/Orbitron-Regular.ttf";
+const PATH_BOLD_FONT: &str = "fonts/static/Orbitron-Bold.ttf";
+const PATH_EXTRA_BOLD_FONT: &str = "fonts/static/Orbitron-ExtraBold.ttf";
+const PATH_IMAGE_PREFIX: &str = "images/";
+
+#[derive(Resource)]
+struct WindowConfig {
+    size: Vec2
+}
 
 #[derive(States, Default, Debug, Hash, PartialEq, Eq, Clone)]
 enum AppState {
     #[default]
+    Initialize,
     Mainmenu,
+    Settings,
+    ChoosePlayer,
     Ingame,
 }
 
@@ -21,24 +37,79 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_state::<AppState>()
         .insert_resource(ClearColor(BACGROUND_COLOR))
+        .insert_resource(WindowConfig {
+            size: Vec2::new(800.0, 600.0),
+        })
+        
         .add_systems(Startup, setup)
         .add_plugins(mainmenu::MainmenuPlugin)
         .run();
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    monitors: Query<&Monitor>,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    mut window_conf: ResMut<WindowConfig>,
+    mut next_state: ResMut<NextState<AppState>>,
 ) {
     println!("main: setup");
+    // assume that there is only one monitor
+    let primary_monitor: bool = false;
+    for monitor in monitors.iter() {
+        if primary_monitor {
+            panic!("This Game doesn't support dual monitor!")
+        }
+        let name = monitor.name.clone().unwrap_or_else(|| "<no name>".into());
+        let size = format!("{}x{}px", monitor.physical_height, monitor.physical_width);
+        window_conf.size = Vec2::new(monitor.physical_width as f32, monitor.physical_height as f32);
+        let hz = monitor
+            .refresh_rate_millihertz
+            .map(|x| format!("{}Hz", x as f32 / 1000.0))
+            .unwrap_or_else(|| "<unknown>".into());
+        let position = format!(
+            "x={} y={}",
+            monitor.physical_position.x, monitor.physical_position.y
+        );
+        let scale = format!("{:.2}", monitor.scale_factor);
+        // show monitor info
+        println!(
+            "Monitor: {} ({}), {} at {}, scale: {}",
+            name, size, hz, position, scale
+        );
+    }
+    // set window config
+    if let Ok(mut window) = windows.get_single_mut() {
+        window.mode = WindowMode::Fullscreen(MonitorSelection::Primary);
+        window.resolution = window_conf.size.into();
+    }
     // camera
     commands.spawn(Camera2d::default());
-    // bgm
-    let bgm_sound = asset_server.load(PATH_SOUND_BGM);
+    next_state.set(AppState::Mainmenu);
+}
 
-    commands.spawn(
-        AudioPlayer::new(
-            bgm_sound
-        )
-    );
+#[cfg(target_arch = "wasm32")]
+fn setup(
+    mut commands: Commands,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    mut window_conf: ResMut<WindowConfig>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
+    println!("main: setup(wasm)");
+    if let Ok(mut window) = windows.get_single_mut() {
+        if let Some(win) = web_sys::window() {
+            if let Ok(screen) = win.screen() {
+                // set window size
+                let width = screen.width().unwrap_or(800) as f32;
+                let height = screen.height().unwrap_or(600) as f32;
+                window.resolution.set(width, height);
+                window_conf.size = Vec2::new(width, height);
+                info!("Set resolution to: {}x{}", width, height);
+            }
+        }
+    }
+    // camera
+    commands.spawn(Camera2d::default());
+    next_state.set(AppState::Mainmenu);
 }
--- src/mainmenu.rs
+++ src/mainmenu.rs
@@ -1,22 +1,19 @@
-use bevy::{
-    prelude::*,
-};
+use bevy::prelude::*;
 
 use crate::{
     GAMETITLE,
-    WINDOW_SIZE,
-    PATH_FONT,
-    AppState
+    //PATH_REGULAR_FONT,
+    PATH_BOLD_FONT,
+    PATH_EXTRA_BOLD_FONT,
+    PATH_IMAGE_PREFIX,
+    AppState,
+    WindowConfig
 };
 
-const GAMETITLE_FONT_SIZE: f32 = 32.0;
+const GAMETITLE_FONT_SIZE: f32 = 100.0;
+const BUTTON_FONT_SIZE: f32 = 50.0;
 const GAMETITLE_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
-const CLICK_START_TEXT: &str = "Click to Start";
-const CLICK_START_FONT_SIZE: f32 = 24.0;
-const CLICK_START_COLOR: Color = Color::srgb(0.2, 0.2, 0.2);
-const TEXT_PADDING: f32 = 20.0;
-const BOARD_SIZE: Vec2 = Vec2::new(280.0, 210.0);
-const BOARD_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
 
 #[derive(Component)]
 struct Mainmenu;
@@ -24,28 +21,175 @@ struct Mainmenu;
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    window_conf: Res<WindowConfig>,
 ) {
     println!("mainmenu: setup");
-    // game title
-    let top = Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 - TEXT_PADDING);
-    commands.spawn(
-        (Text::new(GAMETITLE),
-        TextFont {
-            font: asset_server.load(PATH_FONT),
-            font_size: GAMETITLE_FONT_SIZE,
+
+    // show background image
+    commands.spawn((
+        Sprite{
+            image: asset_server.load(format!("{}background.png", PATH_IMAGE_PREFIX)),
+            custom_size: Some(window_conf.size.into()),
             ..Default::default()
         },
-        TextColor(GAMETITLE_COLOR),
-        TextLayout {
-            justify: JustifyText::Center,
-            ..Default::default()
-        },
+        Mainmenu
     ));
+
+    // bgm
+    commands.spawn((
+        AudioPlayer::new(asset_server.load(PATH_SOUND_BGM)),
+        PlaybackSettings::LOOP.with_spatial(true),
+        GlobalTransform::default(),
+        Mainmenu,
+    ));
+
+    commands
+        .spawn((Node {
+            width: Val::Percent(80.0),
+            height: Val::Percent(80.0),
+            flex_direction: FlexDirection::Column,
+            align_self: AlignSelf::Center,
+            justify_self: JustifySelf::Center,
+            align_items: AlignItems::Center,
+            justify_items: JustifyItems::Center,
+            ..default()
+        }, BackgroundColor(Color::srgb(0.1, 0.5, 0.1)),))
+        .with_children(|parent| {
+            // game title
+            parent.spawn(
+                    (Text::new(GAMETITLE),
+                    TextFont {
+                        font: asset_server.load(PATH_EXTRA_BOLD_FONT),
+                        font_size: GAMETITLE_FONT_SIZE,
+                        ..Default::default()
+                    },
+                    TextColor(GAMETITLE_COLOR),
+                    TextLayout::new_with_justify(JustifyText::Center),
+                    // Set the style of the Node itself.
+                    Node {
+                        align_self: AlignSelf::Center,
+                        justify_self: JustifySelf::Center,
+                        margin: UiRect{
+                            left: Val::Px(0.0),
+                            right: Val::Px(0.0),
+                            top: Val::Px(0.0),
+                            bottom: Val::Percent(20.0),
+                        },
+                        ..default()
+                    },
+                    Mainmenu
+                ));
+            parent.spawn((
+                    Button,
+                    Node {
+                        width: Val::Percent(50.0),
+                        height: Val::Percent(10.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        margin: UiRect::all(Val::Percent(1.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BackgroundColor(Color::srgb(0.1, 0.1, 0.1)),
+                    Mainmenu
+                ))
+                .with_child((
+                    Text::new("Start"),
+                    TextFont {
+                        font: asset_server.load(PATH_BOLD_FONT),
+                        font_size: BUTTON_FONT_SIZE,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+                parent.spawn((
+                    Button,
+                    Node {
+                        width: Val::Percent(50.0),
+                        height: Val::Percent(10.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        margin: UiRect::all(Val::Percent(1.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BackgroundColor(Color::srgb(0.1, 0.1, 0.1)),
+                    Mainmenu
+                ))
+                .with_child((
+                    Text::new("Settings"),
+                    TextFont {
+                        font: asset_server.load(PATH_BOLD_FONT),
+                        font_size: BUTTON_FONT_SIZE,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+                parent.spawn((
+                    Button,
+                    Node {
+                        width: Val::Percent(50.0),
+                        height: Val::Percent(10.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        margin: UiRect::all(Val::Percent(1.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BorderColor(Color::BLACK),
+                    BackgroundColor(Color::srgb(0.1, 0.1, 0.1)),
+                    Mainmenu
+                ))
+                .with_child((
+                    Text::new("Exit"),
+                    TextFont {
+                        font: asset_server.load(PATH_BOLD_FONT),
+                        font_size: BUTTON_FONT_SIZE,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
+        });
+
 }
 
-fn update() {}
+fn update(
+    interaction_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<Button>)>,
+    mut next_state: ResMut<NextState<AppState>>,
+    mut app_exit_events: ResMut<Events<bevy::app::AppExit>>,
+    text_query: Query<&Text>,
+) {
+    for (interaction, children) in &mut interaction_query.iter() {
+        match *interaction {
+            Interaction::Pressed => {
+                if children.len() > 0 {
+                    let text = text_query.get(children[0]).unwrap();
+                    match text.0.as_str() {
+                        "Start" => {
+                            next_state.set(AppState::ChoosePlayer);
+                        }
+                        "Settings" => {
+                            next_state.set(AppState::Settings);
+                        }
+                        "Exit" => {
+                            app_exit_events.send(AppExit::Success);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+}
 
 pub struct MainmenuPlugin;
 
