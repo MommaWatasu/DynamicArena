6bdf612d00c2a316ec864f4821577c8240e487a8
Momma Watasu <ascendwatson@gmail.com>
2025-07-11 10:19:58 +0900

    [add] add code for controller
    now controller is available to operate player

--- src/ingame/player.rs
+++ src/ingame/player.rs
@@ -686,6 +686,11 @@ fn keyboard_input(
     config: Res<GameConfig>,
     mut player_query: Query<(&mut Player, &PlayerID)>,
 ) {
+    if config.gamepads[0] != Entity::from_raw(0) {
+        // if gamepad is enabled, we don't handle keyboard input
+        return;
+    }
+
     for (mut player, player_id) in player_query.iter_mut() {
         // skip player 1(opponent) in order to control player 0
         // this is for debugging purpose
@@ -761,11 +766,6 @@ fn keyboard_input(
                     * CHARACTER_PROFILES[player.character_id as usize].agility * 2.0;
                 player.velocity = Vec2::new(x_vel, 0.0);
             }
-        } else if player.state.check(PlayerState::BEND_DOWN) {
-            // player is bending down
-            // then stop bending down
-            player.state &= !PlayerState::BEND_DOWN;
-            player.set_animation(IDLE_POSE1, 0, 10);
         }
         if keys.just_pressed(KeyCode::Space) {
             if player.state.is_idle() {
